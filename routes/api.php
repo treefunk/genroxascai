8313a374477ee2a2be39fdf6c76947fec11e9b31
KorgKurzweil <korgkurzweil@gmail.com>
2019-02-18 01:49:12 +0800

    module, lesson api

--- routes/api.php
+++ routes/api.php
@@ -12,24 +12,30 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
+  Route::post('logout', 'Auth\LoginController@logout');
 
-    Route::get('/user', 'UserController@fetchAuthUser');
+  Route::get('/user', 'UserController@fetchAuthUser');
 
-
-
-    Route::patch('settings/profile', 'Settings\ProfileController@update');
-    Route::patch('settings/password', 'Settings\PasswordController@update');
+  Route::patch('settings/profile', 'Settings\ProfileController@update');
+  Route::patch('settings/password', 'Settings\PasswordController@update');
 });
 
 Route::group(['middleware' => 'guest:api'], function () {
-    Route::post('login', 'Auth\LoginController@login');
-    Route::post('register', 'Auth\RegisterController@register');
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+  Route::post('login', 'Auth\LoginController@login');
+  Route::post('register', 'Auth\RegisterController@register');
+  Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+  Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
-    Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
-    Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+  Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
+  Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+});
+
+Route::group(['middleware' => 'auth:api'], function () {
+  Route::apiResources([
+    'modules' => 'Api\ModuleController',
+    'lessons' => 'Api\LessonController'
+  ]);
 });
