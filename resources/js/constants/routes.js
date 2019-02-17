4fedcdb27227322e6a2e5c36349e002309018df7
KorgKurzweil <korgkurzweil@gmail.com>
2019-02-18 02:50:37 +0800

    route constants

---
+++ resources/js/constants/routes.js
@@ -0,0 +1,13 @@
+export const ROUTE_NAMES = {
+  HOME: 'home',
+  LOGIN: 'login',
+  WELCOME: 'welcome',
+  PASSWORD_REQUEST: 'password.request',
+  PASSWORD_RESET: 'password.reset',
+  DASHBOARD: 'dashboard',
+  SETTINGS: 'settings',
+  SETTINGS_PROFILE: 'settings.profile',
+  SETTINGS_PASSWORD: 'settings.password',
+  MODULES: 'modules',
+  LESSONS: 'lessons'
+}
--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,3 +1,5 @@
+import { ROUTE_NAMES } from '~/constants'
+
 const Welcome = () => import('~/pages/welcome').then(m => m.default || m)
 const Login = () => import('~/pages/auth/login').then(m => m.default || m)
 const PasswordEmail = () => import('~/pages/auth/password/email').then(m => m.default || m)
@@ -10,22 +12,26 @@ const SettingsProfile = () => import('~/pages/settings/profile').then(m => m.def
 const SettingsPassword = () => import('~/pages/settings/password').then(m => m.default || m)
 
 const Dashboard = () => import('~/pages/Dashboard').then(m => m.default || m)
+const Modules = () => import('~/pages/modules/index').then(m => m.default || m)
+const Lessons = () => import('~/pages/lessons/index').then(m => m.default || m)
 
 export default [
-  { path: '/', name: 'welcome', component: Welcome },
+  { path: '/', name: ROUTE_NAMES.WELCOME, component: Welcome },
 
-  { path: '/login', name: 'login', component: Login },
-  { path: '/password/reset', name: 'password.request', component: PasswordEmail },
-  { path: '/password/reset/:token', name: 'password.reset', component: PasswordReset },
+  { path: '/login', name: ROUTE_NAMES.LOGIN, component: Login },
+  { path: '/password/reset', name: ROUTE_NAMES.PASSWORD_REQUEST, component: PasswordEmail },
+  { path: '/password/reset/:token', name: ROUTE_NAMES.PASSWORD_RESET, component: PasswordReset },
 
-  { path: '/home', name: 'home', component: Home },
-  { path: '/dashboard', name: 'dashboard', component: Dashboard },
+  { path: '/home', name: ROUTE_NAMES.HOME, component: Home },
+  { path: '/dashboard', name: ROUTE_NAMES.DASHBOARD, component: Dashboard },
+  { path: '/modules', name: ROUTE_NAMES.MODULES, component: Modules },
+  { path: '/lessons', name: ROUTE_NAMES.LESSONS, component: Lessons },
   { path: '/settings',
     component: Settings,
     children: [
-      { path: '', redirect: { name: 'settings.profile' } },
-      { path: 'profile', name: 'settings.profile', component: SettingsProfile },
-      { path: 'password', name: 'settings.password', component: SettingsPassword }
+      { path: '', redirect: { name: ROUTE_NAMES.SETTINGS_PROFILE } },
+      { path: 'profile', name: ROUTE_NAMES.SETTINGS_PROFILE, component: SettingsProfile },
+      { path: 'password', name: ROUTE_NAMES.SETTINGS_PASSWORD, component: SettingsPassword }
     ] },
 
   { path: '*', component: NotFound }
