f67ecf94304c3dc9033ea0252cb83ee6681f69bf
KorgKurzweil <korgkurzweil@gmail.com>
2019-02-18 03:36:38 +0800

    added description to lesson seeder

--- database/seeds/LessonSeeder.php
+++ database/seeds/LessonSeeder.php
@@ -12,11 +12,16 @@ class LessonSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create();
         $modules = Module::all();
 
         foreach($modules as $key => $module){
             for($x = 0; $x < 6; $x++){
-                factory('App\Lesson')->create(['module_id' => $module->id,'order' => $x + 1]);
+                factory('App\Lesson')->create([
+                  'module_id' => $module->id,
+                  'order' => $x + 1,
+                  'description' => $faker->text(200)
+                ]);
             }
         }
     }
