6633083554bea6b201ebec2885d9306cfbd2fb8b
KorgKurzweil <korgkurzweil@gmail.com>
2019-02-20 04:39:01 +0800

    show name on top nav

--- resources/views/layouts/partials/top-nav.blade.php
+++ resources/views/layouts/partials/top-nav.blade.php
@@ -18,16 +18,16 @@
       </div>
     </form> -->
 
+    <div class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
+    </div>
+
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
-      <li class="nav-item dropdown no-arrow">
+      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          <i class="fas fa-user-circle fa-fw"></i>
+          {{ Auth::user()->firstname }}  {{ Auth::user()->lastname }}
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-          <a class="dropdown-item" href="#">Settings</a>
-          <a class="dropdown-item" href="#">Activity Log</a>
-          <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
         </div>
       </li>
