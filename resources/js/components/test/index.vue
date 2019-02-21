a3ad21f9e0b36b1ab24368f9d7de5253e3d9d85a
KorgKurzweil <korgkurzweil@gmail.com>
2019-02-21 11:22:22 +0800

    refactor test to component

---
+++ resources/js/components/test/index.vue
@@ -0,0 +1,93 @@
+<template>
+	<div>
+    <transition name="slideRight">
+      <p v-if="isTestClosed()">
+        Pre test is not open as of the moment
+      </p>
+    </transition>
+
+    <transition name="bounce">
+      <div v-if="canStartTest()" class="text-center">
+        <h4>Press Start when you are ready to take the Test</h4>
+     		<div v-if="user.gender === 'female'">
+          <img src="" class="img img-responsive full-width" src="/images/cliparts/goodluck-girl.svg" />
+        </div>
+        <div v-else>
+        	<img src="" class="img img-responsive full-width" src="/images/cliparts/goodluck-boy.svg" />
+        </div>
+
+        <button v-if="isContinuation()" type="button" class="btn btn-primary btn-lg">Continue {{ getTestTypeName() }}</button>
+        <button v-else type="button" class="btn btn-primary btn-lg">Start {{ getTestTypeName() }}</button>
+      </div>
+    </transition>
+	</div>
+</template>
+<script>
+  import * as _ from 'lodash'
+  import { mapGetters } from 'vuex'
+  import { TEST_STATUS_TYPES, TEST_TYPES } from '~/constants'
+
+export default {
+	props: ['test_type'],
+	computed: mapGetters({
+	  user: 'auth/user',
+	  lesson: 'lesson/lesson',
+	  user_tests: 'user_test/all',
+	  test: 'test/test',
+	}),
+	methods: {
+		getTestTypeName() {
+			return this.isPreTest() ? 'Pre Test' : 'Post Test'
+		},
+		isPreTest() {
+			return this.test_type === TEST_TYPES.PRETEST
+		},
+		isTestClosed () {
+			return !_.get(this.test, 'is_open') && this.test
+		},
+		canStartTest() {
+			if (_.isNull(this.user_tests)) {
+				return false
+			}
+			if (_.size(this.user_tests) === 0 && this.user_tests && _.get(this.test, 'is_open')) {
+				return true
+			}
+			const startedTest = this.getStartedTest()
+			return !startedTest
+		},
+		getStartedTest() {
+			return _.find(this.user_tests, test => {
+				return _.get(test, 'status') === TEST_STATUS_TYPES.STARTED
+			})
+		},
+		isContinuation() {
+			return this.getStartedTest() !== undefined
+		},
+		hasRemainingTry() {
+			return _.get(this.test, 'limit') > _.size(this.user_tests)
+		}
+	},
+  async mounted () {
+  	await this.$store.dispatch('test/clear')
+		await this.$store.dispatch('lesson/clear')
+  	await this.$store.dispatch('user_test/clear')
+	  await this.$store.dispatch('module/clear')
+	  
+    await this.$store.dispatch('module/get', {
+      id: _.get(this.$route.params, 'module_id')
+    });
+  	await this.$store.dispatch('lesson/get', {
+		  id: _.get(this.$route.params, 'lesson_id')
+		});
+		await this.$store.dispatch('test/get', {
+		  id: _.get(this.$route.params, 'lesson_id')
+		});
+		if (_.get(this.test, 'is_open')) {
+			await this.$store.dispatch('user_test/fetch', {
+			  lesson_id: _.get(this.$route.params, 'lesson_id'),
+			  type: this.test_type
+			});
+		}
+  }
+}
+</script>
--- resources/js/pages/posttests/index.vue
+++ resources/js/pages/posttests/index.vue
@@ -1,10 +1,31 @@
 <template>
 	<div>
-		Post Tests page is under construction
+	<app-breadcrumbs></app-breadcrumbs>
+    <app-test :test_type="getTestType()"></app-test>
 	</div>
 </template>
 <script>
+  import { TEST_TYPES } from '~/constants'
+  import Breadcrumbs from '~/components/breadcrumbs/index'
+  import Test from '~/components/test/index'
+
 export default {
-	
+	metaInfo() {
+	  return { title: 'Post Test' };
+	},
+	watch: {
+	},
+	components: {
+	  'app-breadcrumbs': Breadcrumbs,
+	  'app-test': Test
+	},
+
+	methods: {
+		getTestType () {
+			return TEST_TYPES.POSTTEST
+		}
+	},
+  async mounted () {
+  }
 }
 </script>
