e7416c187340e6a1d5aed1af4a0ac28a2ee36cfb
KorgKurzweil <korgkurzweil@gmail.com>
2019-03-07 23:03:32 +0800

    seed

---
+++ database/seeds/SeedNoStudentTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SeedNoStudentTest extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(RolesSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(ModulesSeeder::class);
+        $this->call(LessonSeeder::class);
+        $this->call(ReviewMaterialSeeder::class);
+        $this->call(QuestionsSeeder::class);
+        $this->call(AttendanceSeeder::class);
+        $this->call(SectionsSeeder::class);
+        $this->call(DrillSeeder::class);
+    }
+}
